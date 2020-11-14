73d2aba7609d9a9435ba14f4bc8948cc389f2896
s40723150 <40723150@gm.nfu.edu.tw>
2020-11-14 15:12:08 +0800

    add soble outline mode

--- codes/main_code.py
+++ codes/main_code.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 # use webcam to catch image
 Vid_cap = cv2.VideoCapture(0)
 Vid_cap.set(3, 640)
@@ -8,34 +8,57 @@ window_name = "outline_filter"
 
 def empty(a):
     pass
-def trackbar_set(window_name, bar1_name, bar2_name):
+def trackbar_set(window_name, bar1_name, bar2_name, bar3_name):
     cv2.namedWindow(window_name)
-    cv2.resizeWindow(window_name, 640, 560)
+    cv2.resizeWindow(window_name, 640, 600)
     cv2.createTrackbar(bar1_name, window_name, 52, 255, empty)
     cv2.createTrackbar(bar2_name, window_name, 50, 255, empty)
-    return bar1_name,bar2_name
+    cv2.createTrackbar(bar3_name, window_name, 3, 3, empty)
+    return bar1_name,bar2_name, bar3_name
 def filter(mode_number):
     GBlur = cv2.GaussianBlur(vid, (7, 7), 0)  # smooth the Vid_Gray
-    Gray = cv2.cvtColor(GBlur, cv2.COLOR_BGR2GRAY)  # convert BGR to gray
-    Canny = cv2.Canny(Gray, c_1, c_2)  # find edges
+    B_Gray = cv2.cvtColor(GBlur, cv2.COLOR_BGR2GRAY)  # convert BGR to gray
+    Canny = cv2.Canny(B_Gray, c_1, c_2)  # find edges
 
-    filter_list = [GBlur, Gray, Canny]
-    mode_number = -1
+    Gray = cv2.cvtColor(vid, cv2.COLOR_BGRA2GRAY)
 
-    Filter = filter_list[mode_number]
-    Outline = frames(vid, Filter)
-    cv2.imshow(window_name, Outline)
-    return Outline
-def frames(vid, Filter):
-    thresh, mask = cv2.threshold(Filter, 240, 255, cv2.THRESH_BINARY)
-    # Use Vid_Canny to make mask
-    F_mask = cv2.bitwise_and(Filter, Filter, mask=mask)
-    F_bgr = cv2.cvtColor(F_mask, cv2.COLOR_GRAY2BGR)
-    Outline = cv2.add(vid, F_bgr)  # mix video with filter
-    return Outline
-def save_img(path, img_name,img_type,  i):
-    set_path = str(path+img_name+str(i)+"."+img_type)
-    cv2.imwrite(set_path, filter(3))
+    sobelX = cv2.Sobel(Gray, cv2.CV_64F, 1, 0)
+    sobelY = cv2.Sobel(Gray, cv2.CV_64F, 0, 1)
+
+    sobelX = np.uint8(np.absolute(sobelX))
+    sobelY = np.uint8(np.absolute(sobelY))
+    Sobel = cv2.bitwise_or(sobelX, sobelY)
+
+    filter_list1 = [GBlur, Gray, Canny, Sobel]
+    filter_list2 = ["GBlur", "Gray", "Canny", "Sobel"]
+
+    Filter = filter_list1[mode_number]
+    if mode_number >= 2:
+        Outline = frames(vid, Filter, mode_number)
+        cv2.imshow(window_name, Outline)
+    else:
+        Outline = Filter
+        cv2.imshow(window_name, Outline)
+    return Outline, filter_list2[mode_number]
+def frames(vid, Filter, mode_number):
+    if mode_number == 2:
+        thresh, mask = cv2.threshold(Filter, 240, 255, cv2.THRESH_BINARY)
+        # Use Vid_Canny to make mask
+        F_mask = cv2.bitwise_and(Filter, Filter, mask=mask)
+        F_bgr = cv2.cvtColor(F_mask, cv2.COLOR_GRAY2BGR)
+        Outline = cv2.add(vid, F_bgr)  # mix video with filter
+        return Outline
+    else:
+        thresh, mask = cv2.threshold(Filter, 50, 255, cv2.THRESH_BINARY)
+        # Use Vid_Canny to make mask
+        F_mask = cv2.bitwise_and(Filter, Filter, mask=mask)
+        F_bgr = cv2.cvtColor(F_mask, cv2.COLOR_GRAY2BGR)
+        Outline = cv2.add(vid, F_bgr)  # mix video with filter
+        return Outline
+
+def save_img(path, img_name,img_type,filter_number, i):
+    set_path = str(path+filter(filter_number)[1]+img_name+str(i)+"."+img_type)
+    cv2.imwrite(set_path, filter(filter_number)[0])
     print(set_path, i)
 '''
 def recorder_avi(file_name, framerate):
@@ -70,7 +93,7 @@ def rec():
             pass
 '''
 
-trackbar = trackbar_set(window_name, "Canny 1", "Canny 2")
+trackbar = trackbar_set(window_name, "Canny 1", "Canny 2", "FilterMode")
 save_path = "./../picture/"
 i = 0
 while True:
@@ -79,15 +102,15 @@ while True:
     F_place = vid[0:rows, 0:cols]
     c_1 = cv2.getTrackbarPos(trackbar[0], window_name)
     c_2 = cv2.getTrackbarPos(trackbar[1], window_name)
-
-    filter(3)
+    filter_number = cv2.getTrackbarPos(trackbar[2], window_name)
+    filter(filter_number)
     key = cv2.waitKey(10) & 0XFF
     esc = 27
     if key == esc:
         break
     elif key == ord("r"):
         i = i + 1
-        save_img(save_path,"picture","png",i)
+        save_img(save_path,"picture","png", filter_number, i)
         continue
     else:
         pass
